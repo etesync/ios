dac3c0a6d2d1a3d52cff0dbdf15e1148800b6373
Tom Hacohen <tom@stosb.com>
2019-02-20 18:57:22 +0000

    Start using native base.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import * as React from 'react';
-import { Text, View, Button } from 'react-native';
-import { Calendar, Permissions } from 'expo';
+import { Container, Header, Left, Right, Body, Title, Content, Button, Text } from 'native-base';
+import { Constants, Font, Calendar, Permissions } from 'expo';
 
 import * as EteSync from './api/EteSync';
 
 import ErrorBoundary from  './ErrorBoundary';
 
 class App extends React.Component {
+  state = {
+    fontLoaded: false
+  };
+
   constructor(props: any) {
     super(props);
     this.onPress = this.onPress.bind(this);
   }
 
+  async componentWillMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
+    if (!this.state.fontLoaded) {
+      return <Text>Loading</Text>
+    }
+
     return (
-      <ErrorBoundary>
-        <View style={{ marginTop: 50 }}>
-          <Text>Hello Expo tom!</Text>
-          <Button title="Click" onPress={this.onPress} />
-        </View>
-      </ErrorBoundary>
+      <Container style={{ paddingTop: Constants.statusBarHeight }}>
+        <Header>
+          <Left />
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <ErrorBoundary>
+            <Text>Hello Expo tom!</Text>
+            <Button onPress={this.onPress}>
+              <Text>Click</Text>
+            </Button>
+          </ErrorBoundary>
+        </Content>
+      </Container>
     );
   }
 
