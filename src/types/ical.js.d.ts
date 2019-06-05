1dcd4a7f0f9afa15e12a41e34963e5ae569489de
Tom Hacohen <tom@stosb.com>
2019-06-05 15:11:54 +0100

    TypeScript: Add missing types.

---
+++ src/types/ical.js.d.ts
@@ -0,0 +1,113 @@
+declare module 'ical.js' {
+  function parse(input: string): any[];
+  class Component {
+    static public fromString(str: string): Component;
+
+    public name: string;
+
+    constructor(jCal: any[] | string, parent?: Component);
+
+    public toJSON(): any[];
+
+    public getFirstSubcomponent(name?: string): Component | null;
+    public getAllSubcomponents(name?: string): Component[];
+
+    public getFirstPropertyValue(name?: string): any;
+
+    public getFirstProperty(name?: string): Property;
+    public getAllProperties(name?: string): Property[];
+
+    public addProperty(property: Property): Property;
+    public addPropertyWithValue(name: string, value: string | number | object): Property;
+
+    public updatePropertyWithValue(name: string, value: string | number | object): Property;
+
+    public removeAllProperties(name?: string): boolean;
+
+    public addSubcomponent(component: Component): Component;
+  }
+
+  class Event {
+    public uid: string;
+    public summary: string;
+    public startDate: Time;
+    public endDate: Time;
+    public description: string;
+    public location: string;
+    public attendees: Property[];
+
+    public component: Component;
+
+    public constructor(component?: Component | null, options?: {strictExceptions: boolean, exepctions: Array<Component | Event>});
+
+    public isRecurring(): boolean;
+    public iterator(startTime?: Time): RecurExpansion;
+  }
+
+  class Property {
+    public name: string;
+    public type: string;
+
+    constructor(jCal: any[] | string, parent?: Component);
+
+    public getFirstValue(): any;
+    public getValues(): any[];
+
+    public setParameter(name: string, value: string | string[]): void;
+    public setValue(value: string | object): void;
+    public toJSON(): any;
+  }
+
+  interface TimeJsonData {
+    year?: number;
+    month?: number;
+    day?: number;
+    hour?: number;
+    minute?: number;
+    second?: number;
+    isDate?: boolean;
+  }
+
+  class Time {
+    static public fromString(str: string): Time;
+    static public fromJSDate(aDate: Date | null, useUTC: boolean): Time;
+    static public fromData(aData: TimeJsonData): Time;
+
+    static public now(): Time;
+
+    public isDate: boolean;
+    public timezone: string;
+    public zone: Timezone;
+
+    constructor(data?: TimeJsonData);
+    public compare(aOther: Time): number;
+
+    public clone(): Time;
+
+    public adjust(
+      aExtraDays: number, aExtraHours: number, aExtraMinutes: number, aExtraSeconds: number, aTimeopt?: Time): void;
+
+    public addDuration(aDuration: Duration): void;
+    public subtractDateTz(aDate: Time): Duration;
+
+    public toJSDate(): Date;
+    public toJSON(): TimeJsonData;
+  }
+
+  class Duration {
+    public days: number;
+  }
+
+  class RecurExpansion {
+    public complete: boolean;
+
+    public next(): Time;
+  }
+
+  class Timezone {
+    static public localTimezone: Timezone;
+    static public convert_time(tt: Time, fromZone: Timezone, toZone: Timezone): Time;
+
+    public tzid: string;
+  }
+}
