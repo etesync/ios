8b635e84e8d9ec75afa512a3034fae448be2cf93
Tom Hacohen <tom@stosb.com>
2019-07-05 14:03:29 +0100

    Store index: disable the store logger more cleanly.

--- src/store/index.ts
+++ src/store/index.ts
@@ -16,8 +16,8 @@ const middleware = [
   promiseMiddleware,
 ];
 
-if (process.env.NODE_ENV === 'development') {
-  // middleware.push(createLogger());
+if (false && __DEV__) {
+  middleware.push(createLogger());
 }
 
 export const store = createStore(
