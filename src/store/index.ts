67529232a86be3dad64d32aab478136cb67f3e5c
Tom Hacohen <tom@stosb.com>
2019-07-05 17:46:08 +0100

    Redux logger: hack around to make it mostly work with RN.

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,6 +6,7 @@ import { createLogger } from 'redux-logger';
 import promiseMiddleware from './promise-middleware';
 
 import reducers from './construct';
+import * as actions from './actions';
 
 // Workaround babel limitation
 export * from './reducers';
@@ -16,8 +17,50 @@ const middleware = [
   promiseMiddleware,
 ];
 
-if (false && __DEV__) {
-  middleware.push(createLogger());
+if (__DEV__) {
+  const ignoreActions = [
+    'persist/PERSIST',
+    'persist/REHYDRATE',
+    actions.setSyncStateJournal.toString(),
+    actions.unsetSyncStateJournal.toString(),
+    actions.setSyncStateEntry.toString(),
+    actions.unsetSyncStateEntry.toString(),
+  ];
+
+  const predicate = (_: any, action: { type: string }) => {
+    return !ignoreActions.includes(action.type);
+  };
+
+  const logger = {
+    log: (msg: string) => {
+      if (msg[0] === '#') {
+        console.log(msg);
+      }
+    },
+  };
+
+  middleware.push(createLogger({
+    predicate,
+    logger,
+    stateTransformer: () => 'state',
+    actionTransformer: ({ type, error, payload }) => ({ type, error, payload: !!payload }),
+    titleFormatter: (action: { type: string, error: any, payload: boolean }, time: string, took: number) => {
+      let prefix = '->';
+      if (action.error) {
+        prefix = 'xx';
+      } else if (action.payload) {
+        prefix = '==';
+      }
+      return `# ${prefix} ${action.type} @ ${time} (in ${took.toFixed(2)} ms)`;
+    },
+    colors: {
+      title: false,
+      prevState: false,
+      action: false,
+      nextState: false,
+      error: false,
+    },
+  }));
 }
 
 export const store = createStore(
