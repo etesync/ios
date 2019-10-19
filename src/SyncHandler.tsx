3c712087f07ff054655e81e460aec7dec7c0b767
Tom Hacohen <tom@stosb.com>
2019-10-19 20:36:56 +0300

    SyncHandler: cache sync entries decryption.
    This speeds things up significantly.

--- src/SyncHandler.tsx
+++ src/SyncHandler.tsx
@@ -9,7 +9,8 @@ import { createSelector } from 'reselect';
 import * as EteSync from './api/EteSync';
 import { byte } from './api/Helpers';
 
-import { JournalsType, EntriesType, StoreState, CredentialsData, UserInfoType } from './store';
+import { store, JournalsType, EntriesType, StoreState, CredentialsData, UserInfoType } from './store';
+import { setSyncInfoCollection, setSyncInfoItem, unsetSyncInfoCollection } from './store/actions';
 
 import { useCredentials } from './login';
 
@@ -36,6 +37,8 @@ export const syncInfoSelector = createSelector(
   (props: SyncInfoSelectorProps) => props.entries,
   (props: SyncInfoSelectorProps) => props.userInfo.value!,
   (etesync, journals, entries, userInfo) => {
+    const syncInfoCollection = store.getState().cache.syncInfoCollection;
+    const syncInfoItem = store.getState().cache.syncInfoItem;
     const derived = etesync.encryptionKey;
     let asymmetricCryptoManager: EteSync.AsymmetricCryptoManager;
     try {
@@ -49,47 +52,59 @@ export const syncInfoSelector = createSelector(
       }
     }
 
-    return journals.reduce(
-      (ret, journal) => {
-        const journalEntries = entries.get(journal.uid);
-        let prevUid: string | null = null;
+    const ret = Map<string, SyncInfoJournal>().asMutable();
+    journals.forEach((journal) => {
+      const journalEntries = entries.get(journal.uid);
+      let prevUid: string | null = null;
 
-        if (!journalEntries || !journalEntries.value) {
-          return ret;
+      if (!journalEntries || !journalEntries.value) {
+        return;
+      }
+
+      let cryptoManager: EteSync.CryptoManager;
+      let derivedJournalKey: byte[];
+      if (journal.key) {
+        if (!asymmetricCryptoManager) {
+          const keyPair = userInfo.getKeyPair(new EteSync.CryptoManager(derived, 'userInfo', userInfo.version));
+          asymmetricCryptoManager = new EteSync.AsymmetricCryptoManager(keyPair);
         }
+        derivedJournalKey = asymmetricCryptoManager.decryptBytes(journal.key);
+        cryptoManager = EteSync.CryptoManager.fromDerivedKey(derivedJournalKey, journal.version);
+      } else {
+        cryptoManager = new EteSync.CryptoManager(derived, journal.uid, journal.version);
+      }
 
-        let cryptoManager: EteSync.CryptoManager;
-        let derivedJournalKey: byte[];
-        if (journal.key) {
-          if (!asymmetricCryptoManager) {
-            const keyPair = userInfo.getKeyPair(new EteSync.CryptoManager(derived, 'userInfo', userInfo.version));
-            asymmetricCryptoManager = new EteSync.AsymmetricCryptoManager(keyPair);
-          }
-          derivedJournalKey = asymmetricCryptoManager.decryptBytes(journal.key);
-          cryptoManager = EteSync.CryptoManager.fromDerivedKey(derivedJournalKey, journal.version);
-        } else {
-          cryptoManager = new EteSync.CryptoManager(derived, journal.uid, journal.version);
+      const collectionInfo = journal.getInfo(cryptoManager);
+      store.dispatch(setSyncInfoCollection(etesync, collectionInfo));
+
+      const syncEntries = journalEntries.value.map((entry: EteSync.Entry) => {
+        const cacheEntry = syncInfoItem.getIn([journal.uid, entry.uid]);
+        if (cacheEntry) {
+          return cacheEntry;
         }
+        const syncEntry = entry.getSyncEntry(cryptoManager, prevUid);
+        prevUid = entry.uid;
 
-        const collectionInfo = journal.getInfo(cryptoManager);
+        store.dispatch(setSyncInfoItem(etesync, journal.uid, syncEntry as any));
+        return syncEntry;
+      });
 
-        const syncEntries = journalEntries.value.map((entry: EteSync.Entry) => {
-          const syncEntry = entry.getSyncEntry(cryptoManager, prevUid);
-          prevUid = entry.uid;
+      ret.set(journal.uid, {
+        entries: syncEntries,
+        collection: collectionInfo,
+        journal,
+        derivedJournalKey,
+        journalEntries: journalEntries.value,
+      });
+    });
 
-          return syncEntry;
-        });
+    for (const collection of syncInfoCollection.values()) {
+      if (!ret.has(collection.uid)) {
+        store.dispatch(unsetSyncInfoCollection(etesync, collection));
+      }
+    }
 
-        return ret.set(journal.uid, {
-          entries: syncEntries,
-          collection: collectionInfo,
-          journal,
-          derivedJournalKey,
-          journalEntries: journalEntries.value,
-        });
-      },
-      Map<string, SyncInfoJournal>()
-    );
+    return ret.asImmutable();
   }
 );
 
