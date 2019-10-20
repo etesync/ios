366e75d2a0e72f363cfd5db7f75094bdbd512bab
Tom Hacohen <tom@stosb.com>
2019-10-20 14:11:28 +0300

    Silence warnings when creating events.
    This is due to bad typings in expo.

--- src/sync/helpers.ts
+++ src/sync/helpers.ts
@@ -114,6 +114,11 @@ export function eventVobjectToNative(event: EventType) {
     alarms: event.component.getAllSubcomponents('valarm').map(eventAlarmVobjectToNative).filter((x) => x) as any,
     recurrenceRule: eventRruleVobjectToNative(event),
     timeZone: event.timezone || '',
+    // Add these read-only elements just to shush types.
+    id: undefined,
+    calendarId: undefined,
+    availability: undefined,
+    status: undefined,
   };
 
   return ret;
