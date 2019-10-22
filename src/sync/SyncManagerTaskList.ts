8a92cbdc4f628a89e7c8b01b55d354fd8e4d292b
Tom Hacohen <tom@stosb.com>
2019-10-22 14:20:35 +0300

    sync reminders: implement pushing reminders.

--- src/sync/SyncManagerTaskList.ts
+++ src/sync/SyncManagerTaskList.ts
@@ -2,6 +2,8 @@ import * as EteSync from '../api/EteSync';
 import * as ICAL from 'ical.js';
 import * as Calendar from 'expo-calendar';
 
+import { logger } from '../logging';
+
 import { SyncInfo } from '../SyncGate';
 import { SyncStateJournalEntryData } from '../store';
 
@@ -15,7 +17,75 @@ export class SyncManagerTaskList extends SyncManagerCalendarBase<TaskType, Nativ
   protected entityType = Calendar.EntityTypes.REMINDER;
 
   protected async syncPush(syncInfo: SyncInfo) {
-    // FIXME: implement
+    const syncStateJournals = this.syncStateJournals;
+    const now = new Date();
+    const dateYearRange = 4; // Maximum year range supported on iOS
+
+    for (const syncJournal of syncInfo.values()) {
+      if (syncJournal.collection.type !== this.collectionType) {
+        continue;
+      }
+
+      const collection = syncJournal.collection;
+      const uid = collection.uid;
+      logger.info(`Pushing ${uid}`);
+
+      const syncStateEntriesReverse = this.syncStateEntries.get(uid).mapEntries((_entry) => {
+        const entry = _entry[1];
+        return [entry.localId, entry];
+      }).asMutable();
+
+      const syncEntries: EteSync.SyncEntry[] = [];
+
+      const syncStateJournal = syncStateJournals.get(uid);
+      const localId = syncStateJournal.localId;
+      for (let i = -2 ; i <= 1 ; i++) {
+        const remindersRangeStart = new Date(new Date().setFullYear(now.getFullYear() + (i * dateYearRange)));
+        const remindersRangeEnd = new Date(new Date().setFullYear(now.getFullYear() + ((i + 1) * dateYearRange)));
+
+        const existingReminders = await Calendar.getRemindersAsync([localId] as any, undefined, remindersRangeStart, remindersRangeEnd);
+
+        existingReminders.forEach((_reminder) => {
+          const syncStateEntry = syncStateEntriesReverse.get(_reminder.id);
+
+          // FIXME: ignore recurring reminders at the moment as they seem to be broken with Expo
+          if (_reminder.recurrenceRule) {
+            return;
+          }
+
+          const reminder = { ..._reminder, uid: (syncStateEntry) ? syncStateEntry.uid : _reminder.id };
+          const syncEntry = this.syncPushHandleAddChange(syncJournal, syncStateEntry, reminder);
+          if (syncEntry) {
+            syncEntries.push(syncEntry);
+          }
+
+          if (syncStateEntry) {
+            syncStateEntriesReverse.delete(syncStateEntry.uid);
+          }
+        });
+      }
+
+      for (const syncStateEntry of syncStateEntriesReverse.values()) {
+        // Deleted
+        let existingReminder: Calendar.Reminder;
+        try {
+          existingReminder = await Calendar.getReminderAsync(syncStateEntry.localId);
+        } catch (e) {
+          // Skip
+        }
+
+        // FIXME: handle the case of the reminder still existing for some reason.
+        if (!existingReminder) {
+          // If the reminder still exists it means it's not deleted.
+          const syncEntry = this.syncPushHandleDeleted(syncJournal, syncStateEntry);
+          if (syncEntry) {
+            syncEntries.push(syncEntry);
+          }
+        }
+      }
+
+      this.pushJournalEntries(syncJournal, syncEntries);
+    }
   }
 
   protected syncEntryToVobject(syncEntry: EteSync.SyncEntry) {
