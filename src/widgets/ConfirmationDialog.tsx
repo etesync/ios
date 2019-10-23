ee51ecafbbd2f53515462557c805125204547bac
Tom Hacohen <tom@stosb.com>
2019-10-23 23:15:24 +0300

    Confirmation dialog: if onOK returns a promise, use it to indicate loading.

--- src/widgets/ConfirmationDialog.tsx
+++ src/widgets/ConfirmationDialog.tsx
@@ -1,24 +1,33 @@
 import * as React from 'react';
 import { Keyboard } from 'react-native';
-import { Portal, Dialog, Button } from 'react-native-paper';
+import { Portal, Dialog, Button, ProgressBar, Paragraph } from 'react-native-paper';
 
 interface PropsType {
   title: string;
   children: React.ReactNode | React.ReactNode[];
   visible: boolean;
   onCancel?: () => void;
-  onOk?: () => void;
+  onOk?: () => void | Promise<any>;
   labelCancel?: string;
   labelOk?: string;
 }
 
 export default React.memo(function ConfirmationDialog(props: PropsType) {
+  const [loading, setLoading] = React.useState(false);
   const labelCancel = props.labelCancel || 'Cancel';
   const labelOk = props.labelOk || 'OK';
 
   React.useMemo(() => {
     Keyboard.dismiss();
   }, [props.visible]);
+
+  function onOk() {
+    const ret = props.onOk();
+    if (ret && ret.then) {
+      // If it's a promise, we update the loading state based on it.
+      setLoading(true);
+    }
+  }
 
   return (
     <Portal>
@@ -28,14 +37,20 @@ export default React.memo(function ConfirmationDialog(props: PropsType) {
       >
         <Dialog.Title>{props.title}</Dialog.Title>
         <Dialog.Content>
-          {props.children}
+          { (loading) ?
+            <>
+              <Paragraph>Loading...</Paragraph>
+              <ProgressBar indeterminate />
+            </> :
+            props.children
+          }
         </Dialog.Content>
         <Dialog.Actions>
           {props.onCancel &&
-            <Button onPress={props.onCancel}>{labelCancel}</Button>
+            <Button disabled={loading} onPress={props.onCancel}>{labelCancel}</Button>
           }
           {props.onOk &&
-            <Button onPress={props.onOk}>{labelOk}</Button>
+            <Button disabled={loading} onPress={onOk}>{labelOk}</Button>
           }
         </Dialog.Actions>
       </Dialog>
