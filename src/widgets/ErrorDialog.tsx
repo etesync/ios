a74507570e6e3756900b2494f1ef502dd8b7405b
Tom Hacohen <tom@stosb.com>
2019-10-24 15:18:25 +0300

    ErrorDialog: add an ErrorDialog widget.

---
+++ src/widgets/ErrorDialog.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Paragraph } from 'react-native-paper';
+
+import ConfirmationDialog from './ConfirmationDialog';
+
+interface PropsType {
+  title?: string;
+  error?: string;
+  onOk?: () => void | Promise<any>;
+  labelOk?: string;
+  loadingText?: string;
+}
+
+export default React.memo(function ErrorDialog(props: PropsType) {
+  return (
+    <ConfirmationDialog
+      title={props.title}
+      visible={!!props.error}
+      onCancel={props.onOk}
+      labelCancel={props.labelOk || 'Ok'}
+      labelOk={props.labelOk}
+      loadingText={props.loadingText}
+    >
+      <Paragraph>
+        {props.error}
+      </Paragraph>
+    </ConfirmationDialog>
+  );
+});
