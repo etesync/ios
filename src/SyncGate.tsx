37627a059aeaf074672333351fdbf49ff83fbb51
Tom Hacohen <tom@stosb.com>
2019-10-27 22:46:41 +0200

    Sync Gate: remove sanity checks for a very significant (~10x) speed boost.
    We were thrashing this function because we were changing the store a lot.
    Even though we were leaving the function early, it was still very resource
    heavy.

--- src/SyncGate.tsx
+++ src/SyncGate.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
-import { Text } from 'react-native';
 
 import { useCredentials } from './login';
 
@@ -15,8 +14,6 @@ export function useSyncGate() {
   const journals = useSelector((state: StoreState) => state.cache.journals);
   const entries = useSelector((state: StoreState) => state.cache.entries);
   const userInfo = useSelector((state: StoreState) => state.cache.userInfo);
-  const syncInfoCollections = useSelector((state: StoreState) => state.cache.syncInfoCollection);
-  const syncInfoEntries = useSelector((state: StoreState) => state.cache.syncInfoItem);
   const syncCount = useSelector((state: StoreState) => state.syncCount);
 
   if ((syncCount > 0) || !etesync || !journals || !entries || !userInfo) {
@@ -24,14 +21,6 @@ export function useSyncGate() {
   }
 
   syncInfoSelector({ etesync, entries, journals, userInfo });
-
-  if ((syncInfoCollections.size !== journals.size)
-    || !syncInfoEntries.every((syncEntries, key) => {
-      return (syncEntries && (syncEntries.size === entries.get(key)?.size));
-    })
-  ) {
-    return (<Text>Encountered an issue while syncing. It should not happen, please inform developers.</Text>);
-  }
 
   return null;
 }
