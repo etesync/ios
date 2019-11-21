85cc396663e9a06ba8adac1f08bcfbe25870ed18
Tom Hacohen <tom@stosb.com>
2019-11-21 14:39:13 +0200

    Logging: only log in dev mode and add log level support infra

--- src/logging.ts
+++ src/logging.ts
@@ -1,18 +1,39 @@
+enum LogLevel {
+  Critical = 1,
+  Warning,
+  Info,
+  Debug,
+}
+
+const logLevel = (__DEV__) ? LogLevel.Debug : LogLevel.Info;
+
+function shouldLog(messageLevel: LogLevel) {
+  return __DEV__ && (messageLevel <= logLevel);
+}
+
 class Logger {
   public debug(message: any) {
-    console.log(message);
+    if (shouldLog(LogLevel.Debug)) {
+      console.log(message);
+    }
   }
 
   public info(message: any) {
-    console.log(message);
+    if (shouldLog(LogLevel.Info)) {
+      console.log(message);
+    }
   }
 
   public warn(message: any) {
-    console.warn(message);
+    if (shouldLog(LogLevel.Warning)) {
+      console.warn(message);
+    }
   }
 
   public critical(message: any) {
-    console.warn(message);
+    if (shouldLog(LogLevel.Critical)) {
+      console.warn(message);
+    }
   }
 }
 
