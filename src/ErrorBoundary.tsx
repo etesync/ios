29f47a114a99b66b14c831bfab2320287aafdfc0
Tom Hacohen <tom@stosb.com>
2019-11-21 12:31:38 +0200

    Error boundary: improve design and add buttons to report/restart.

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
-import { ScrollView } from 'react-native';
+import { ScrollView, View, Linking } from 'react-native';
+import { Button, Title, Text } from 'react-native-paper';
+
+import { Updates } from 'expo';
 
 import { StoreState } from './store';
 
-import PrettyError from './PrettyError';
-
 import { logger } from './logging';
+import Container from './widgets/Container';
+import { expo } from '../app.json';
+import * as C from './constants';
+
+function emailDevelopers(error: Error) {
+  const subject = encodeURIComponent('EteSync iOS: Crash Report');
+  const bodyJson = {
+    version: expo.version,
+    error: {
+      message: error.message,
+      stack: error.stack?.toString(),
+    },
+  };
+  const body = encodeURIComponent(JSON.stringify(bodyJson));
+  Linking.openURL(`mailto:${C.reportsEmail}?subject=${subject}&body=${body}`);
+}
 
 function ErrorBoundaryInner(props: any) {
   const errors = useSelector((state: StoreState) => state.errors);
@@ -15,7 +32,15 @@ function ErrorBoundaryInner(props: any) {
     logger.critical(error);
     return (
       <ScrollView>
-        <PrettyError error={error} />
+        <Container>
+          <Title>Something went wrong!</Title>
+          <Text selectable>{props.error.message}</Text>
+          <View style={{ marginVertical: 20, flexDirection: 'row', justifyContent: 'space-evenly' }}>
+            <Button mode="contained" onPress={() => emailDevelopers(error)}>Report Bug</Button>
+            <Button mode="contained" onPress={() => Updates.reloadFromCache()}>Reload App</Button>
+          </View>
+          <Text selectable>{props.error.stack}</Text>
+        </Container>
       </ScrollView>
     );
   }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,7 @@ export const dashboard = homePage + 'dashboard/';
 export const sourceCode = 'https://github.com/etesync/etesync-web';
 export const reportIssue = sourceCode + '/issues';
 export const contactEmail = 'contact-ios@etesync.com';
+export const reportsEmail = 'reports-ios@etesync.com';
 
 export const forgotPassword = 'https://www.etesync.com/accounts/password/reset/';
 
