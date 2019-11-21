29876ff6aad6fc4691b00e40baf1f958e9245e0d
Tom Hacohen <tom@stosb.com>
2019-11-21 19:48:13 +0200

    LoadingIndicator: wrap inside a container so we use the theme's background.

--- src/widgets/LoadingIndicator.tsx
+++ src/widgets/LoadingIndicator.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import { ActivityIndicator } from 'react-native-paper';
 
+import Container from './Container';
+
 export default function LoadingIndicator(_props: any) {
   const { style, ...props } = _props;
   return (
-    <ActivityIndicator animating size="large" style={{ margin: 15, ...style }} {...props} />
+    <Container style={{ flexGrow: 1 }}>
+      <ActivityIndicator animating size="large" style={{ margin: 15, ...style }} {...props} />
+    </Container>
   );
 }
