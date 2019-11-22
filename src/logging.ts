e75ef7d2363b66bfaa9e7d41058dbe5dd082040b
Tom Hacohen <tom@stosb.com>
2019-11-22 18:44:28 +0200

    Logging: support turning off and implement logging to storage.

--- src/logging.ts
+++ src/logging.ts
@@ -1,39 +1,88 @@
-enum LogLevel {
-  Critical = 1,
+import { AsyncStorage } from 'react-native';
+
+export enum LogLevel {
+  Off = 0,
+  Critical,
   Warning,
   Info,
   Debug,
 }
 
-const logLevel = (__DEV__) ? LogLevel.Debug : LogLevel.Info;
+let logLevel = (__DEV__) ? LogLevel.Debug : LogLevel.Off;
+
+export function setLogLevel(level: LogLevel) {
+  logLevel = level;
+}
 
 function shouldLog(messageLevel: LogLevel) {
-  return __DEV__ && (messageLevel <= logLevel);
+  return messageLevel <= logLevel;
 }
+
+function logPrint(messageLevel: LogLevel, message: any) {
+  if (!shouldLog(messageLevel)) {
+    return;
+  }
+
+  switch (messageLevel) {
+    case LogLevel.Critical:
+    case LogLevel.Warning:
+      console.warn(message);
+      break;
+    default:
+      console.log(message);
+  }
+}
+
+const logIdentifier = '__logging_';
+
+function logToBuffer(messageLevel: LogLevel, message: any) {
+  if (!shouldLog(messageLevel)) {
+    return;
+  }
+
+  AsyncStorage.setItem(`${logIdentifier}${new Date()}`, `${messageLevel}: ${message}`);
+}
+
+async function getLogKeys() {
+  const keys = await AsyncStorage.getAllKeys();
+  return keys.filter((key) => key.startsWith(logIdentifier));
+}
+
+export async function getLogs() {
+  const wantedKeys = await getLogKeys();
+  if (wantedKeys.length === 0) {
+    return [];
+  }
+
+  const wantedItems = await AsyncStorage.multiGet(wantedKeys);
+  return wantedItems.map(([_key, value]) => value);
+}
+
+export async function clearLogs() {
+  const wantedKeys = await getLogKeys();
+  if (wantedKeys.length === 0) {
+    return;
+  }
+  await AsyncStorage.multiRemove(wantedKeys);
+}
+
+const logHandler = (__DEV__) ? logPrint : logToBuffer;
 
 class Logger {
   public debug(message: any) {
-    if (shouldLog(LogLevel.Debug)) {
-      console.log(message);
-    }
+    logHandler(LogLevel.Debug, message);
   }
 
   public info(message: any) {
-    if (shouldLog(LogLevel.Info)) {
-      console.log(message);
-    }
+    logHandler(LogLevel.Info, message);
   }
 
   public warn(message: any) {
-    if (shouldLog(LogLevel.Warning)) {
-      console.warn(message);
-    }
+    logHandler(LogLevel.Warning, message);
   }
 
   public critical(message: any) {
-    if (shouldLog(LogLevel.Critical)) {
-      console.warn(message);
-    }
+    logHandler(LogLevel.Critical, message);
   }
 }
 
