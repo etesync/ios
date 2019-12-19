495fdb3913081b0c420cc281b43cd87401fe33f8
Tom Hacohen <tom@stosb.com>
2019-12-19 13:31:50 +0200

    LoadingIndicator: support optional status string and improve design.

--- src/widgets/LoadingIndicator.tsx
+++ src/widgets/LoadingIndicator.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { ActivityIndicator } from 'react-native-paper';
+import { ActivityIndicator, Text } from 'react-native-paper';
 
 import Container from './Container';
 
 export default function LoadingIndicator(_props: any) {
-  const { style, ...props } = _props;
+  const { style, status, ...props } = _props;
   return (
-    <Container style={{ flexGrow: 1 }}>
-      <ActivityIndicator animating size="large" style={{ margin: 15, ...style }} {...props} />
+    <Container style={{ flexGrow: 1, justifyContent: 'center' }}>
+      <ActivityIndicator animating size="large" style={style} {...props} />
+      {status && <Text style={{ textAlign: 'center', marginTop: 30 }}>{status}</Text>}
     </Container>
   );
 }
