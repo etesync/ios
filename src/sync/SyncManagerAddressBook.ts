5bdf77b682a5c0a784ab00aef60f0ba585f842f3
Tom Hacohen <tom@stosb.com>
2019-12-23 12:39:56 +0200

    Sync contacts: implement pushing and cleanup some of the code.

--- src/sync/SyncManagerAddressBook.ts
+++ src/sync/SyncManagerAddressBook.ts
@@ -7,10 +7,10 @@ import { logger } from '../logging';
 import { store, SyncStateJournalEntryData } from '../store';
 import { unsetSyncStateJournal } from '../store/actions';
 
-import { contactVobjectToNative, entryNativeHashCalc, NativeContact } from './helpers';
+import { contactVobjectToNative, entryNativeHashCalc, NativeContact, contactNativeToVobject } from './helpers';
 import { ContactType } from '../pim-types';
 
-import { SyncManagerBase } from './SyncManagerBase';
+import { SyncManagerBase, PushEntry } from './SyncManagerBase';
 
 export class SyncManagerAddressBook extends SyncManagerBase<ContactType, NativeContact> {
   protected collectionType = 'ADDRESS_BOOK';
@@ -34,7 +34,7 @@ export class SyncManagerAddressBook extends SyncManagerBase<ContactType, NativeC
     const storeState = store.getState();
     const syncStateJournals = storeState.sync.stateJournals;
 
-    const contacts = (await Contacts.getContactsAsync()).data;
+    const contacts = (await Contacts.getContactsAsync({ containerId: this.containerId, rawContacts: true })).data;
     for (const contact of contacts) {
       logger.info(`Deleting ${contact.id}`);
       await Contacts.removeContactAsync(contact.id);
@@ -52,15 +52,75 @@ export class SyncManagerAddressBook extends SyncManagerBase<ContactType, NativeC
   }
 
   protected async syncPush() {
-    //
+    const storeState = store.getState();
+    const syncInfoCollections = storeState.cache.syncInfoCollection;
+    const syncStateJournals = storeState.sync.stateJournals;
+    const syncStateEntries = storeState.sync.stateEntries;
+
+    for (const collection of syncInfoCollections.values()) {
+      const uid = collection.uid;
+
+      if (collection.type !== this.collectionType) {
+        continue;
+      }
+
+      logger.info(`Pushing ${uid}`);
+
+      const syncStateEntriesReverse = syncStateEntries.get(uid)!.mapEntries((_entry) => {
+        const entry = _entry[1];
+        return [entry.localId, entry];
+      }).asMutable();
+
+      const pushEntries: PushEntry[] = [];
+
+      const syncStateJournal = syncStateJournals.get(uid)!;
+      // FIXME: add new contacts to the default address book (group)
+      // const localId = syncStateJournal.localId;
+
+      const existingContacts = (await Contacts.getContactsAsync({ containerId: this.containerId, rawContacts: true })).data;
+      existingContacts.forEach((_contact) => {
+        const syncStateEntry = syncStateEntriesReverse.get(_contact.id);
+
+        const contact = { ..._contact, id: _contact.id, uid: (syncStateEntry) ? syncStateEntry.uid : _contact.id };
+        const pushEntry = this.syncPushHandleAddChange(syncStateJournal, syncStateEntry, contact);
+        if (pushEntry) {
+          pushEntries.push(pushEntry);
+        }
+
+        if (syncStateEntry) {
+          syncStateEntriesReverse.delete(syncStateEntry.uid);
+        }
+      });
+
+      for (const syncStateEntry of syncStateEntriesReverse.values()) {
+        // Deleted
+        let existingContact: Contacts.Contact | undefined;
+        try {
+          existingContact = await Contacts.getContactByIdAsync(syncStateEntry.localId);
+        } catch (e) {
+          // Skip
+        }
+
+        // FIXME: handle the case of the contact still existing for some reason.
+        if (!existingContact) {
+          // If the event still exists it means it's not deleted.
+          const pushEntry = this.syncPushHandleDeleted(syncStateJournal, syncStateEntry);
+          if (pushEntry) {
+            pushEntries.push(pushEntry);
+          }
+        }
+      }
+
+      await this.pushJournalEntries(syncStateJournal, pushEntries);
+    }
   }
 
   protected syncEntryToVobject(syncEntry: EteSync.SyncEntry) {
     return new ContactType(new ICAL.Component(ICAL.parse(syncEntry.content)));
   }
 
-  protected nativeToVobject(_nativeItem: NativeContact) {
-    return {} as ContactType;
+  protected nativeToVobject(nativeItem: NativeContact) {
+    return contactNativeToVobject(nativeItem);
   }
 
   protected nativeHashCalc(contact: NativeContact) {
@@ -77,9 +137,7 @@ export class SyncManagerAddressBook extends SyncManagerBase<ContactType, NativeC
         let contactExists = false;
         try {
           if (syncStateEntry) {
-            contactExists = (await Contacts.getContactsAsync({
-              id: syncStateEntry.localId,
-            })).data.length > 0;
+            contactExists = !!(await Contacts.getContactByIdAsync(syncStateEntry.localId));
           }
         } catch (e) {
           // Skip
@@ -98,7 +156,7 @@ export class SyncManagerAddressBook extends SyncManagerBase<ContactType, NativeC
           await Contacts.addExistingContactToGroupAsync(localEntryId, containerLocalId);
         }
 
-        const createdContact = { ...(await Contacts.getContactsAsync({ id: syncStateEntry.localId })).data[0], uid: nativeContact.uid };
+        const createdContact = { ...(await Contacts.getContactByIdAsync(syncStateEntry.localId))!, uid: nativeContact.uid };
         syncStateEntry.lastHash = this.nativeHashCalc(createdContact);
 
         break;
