78c0cbc5ac8132e5cd3676ed2d198f7b2c7c4f44
Tom Hacohen <tom@stosb.com>
2019-12-31 13:41:14 +0000

    NativeCode: Add a concurrent task queue

--- ios/etesync/modules/EtesyncNative.swift
+++ ios/etesync/modules/EtesyncNative.swift
@@ -3,6 +3,8 @@ import EventKit
 
 @objc(EteSyncNative)
 class EteSyncNative: NSObject {
+    let taskQueue = DispatchQueue(label: "com.etesync.DispatchQueue", attributes: .concurrent)
+    
     @objc(hashEvent:resolve:reject:)
     func hashEvent(eventId: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
         let store = EKEventStore()
@@ -14,35 +16,37 @@ class EteSyncNative: NSObject {
     }
     
     @objc(calculateHashesForEvents:from:to:resolve:reject:)
-    func calculateHashesForEvents(calendarId: String, from: NSNumber, to: NSNumber, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
-        let store = EKEventStore()
-        guard let cal = store.calendar(withIdentifier: calendarId) else {
-            reject("no_calendar", String(format: "Calendar with identifier %@ not found", calendarId), nil)
-            return
-        }
-        
-        let start = Date(timeIntervalSince1970: TimeInterval(from.doubleValue))
-        let end = Date(timeIntervalSince1970: TimeInterval(to.doubleValue))
-        
-        let predicate = store.predicateForEvents(withStart: start, end: end, calendars: [cal])
-        
-        let events = store.events(matching: predicate)
-        
-        var handled = Set<String>()
-        var ret: [Any] = []
-        
-        for event in events {
-            if (handled.contains(event.calendarItemIdentifier)) {
-                continue
+    func calculateHashesForEvents(calendarId: String, from: NSNumber, to: NSNumber, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+        taskQueue.async {
+            let store = EKEventStore()
+            guard let cal = store.calendar(withIdentifier: calendarId) else {
+                reject("no_calendar", String(format: "Calendar with identifier %@ not found", calendarId), nil)
+                return
             }
-            handled.insert(event.calendarItemIdentifier)
             
-            ret.append([
-                event.calendarItemIdentifier,
-                etesync.hashEvent(event: event)
-            ])
+            let start = Date(timeIntervalSince1970: TimeInterval(from.doubleValue))
+            let end = Date(timeIntervalSince1970: TimeInterval(to.doubleValue))
+            
+            let predicate = store.predicateForEvents(withStart: start, end: end, calendars: [cal])
+            
+            let events = store.events(matching: predicate)
+            
+            var handled = Set<String>()
+            var ret: [Any] = []
+            
+            for event in events {
+                if (handled.contains(event.calendarItemIdentifier)) {
+                    continue
+                }
+                handled.insert(event.calendarItemIdentifier)
+                
+                ret.append([
+                    event.calendarItemIdentifier,
+                    etesync.hashEvent(event: event)
+                ])
+            }
+            
+            resolve(ret)
         }
-        
-        resolve(ret)
     }
 }
