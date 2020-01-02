36397da9b29271d0f5d34373ec03533c6840f463
Tom Hacohen <tom@stosb.com>
2020-01-02 00:48:46 +0000

    NativeModule: change contact save to only modify passed values.
    This is consistent with Expo.

--- ios/etesync/modules/ContactSerializer.swift
+++ ios/etesync/modules/ContactSerializer.swift
@@ -62,21 +62,51 @@ func decodeDate(data: Dictionary<String, Any>?) -> DateComponents? {
 }
 
 func mutateContact(contact: CNMutableContact, data: Dictionary<String, Any>) {
-    contact.givenName = data[EXContactsKeyFirstName] as! String? ?? ""
-    contact.familyName = data[EXContactsKeyLastName] as! String? ?? ""
-    contact.middleName = data[EXContactsKeyMiddleName] as! String? ?? ""
-    contact.previousFamilyName = data[EXContactsKeyMaidenName] as! String? ?? ""
-    contact.nickname = data[EXContactsKeyNickname] as! String? ?? ""
-    contact.organizationName = data[EXContactsKeyCompany] as! String? ?? ""
-    contact.jobTitle = data[EXContactsKeyJobTitle] as! String? ?? ""
-    contact.departmentName = data[EXContactsKeyDepartment] as! String? ?? ""
-    contact.namePrefix = data[EXContactsKeyNamePrefix] as! String? ?? ""
-    contact.nameSuffix = data[EXContactsKeyNameSuffix] as! String? ?? ""
-    contact.phoneticGivenName = data[EXContactsKeyPhoneticFirstName] as! String? ?? ""
-    contact.phoneticMiddleName = data[EXContactsKeyPhoneticMiddleName] as! String? ?? ""
-    contact.phoneticFamilyName = data[EXContactsKeyPhoneticLastName] as! String? ?? ""
-    contact.note = data[EXContactsKeyNote] as! String? ?? ""
-    contact.birthday = decodeDate(data: data[EXContactsKeyBirthday] as! Dictionary<String, Any>?)
+    if let givenName = data[EXContactsKeyFirstName] as! String? {
+        contact.givenName = givenName
+    }
+    if let familyName = data[EXContactsKeyLastName] as! String? {
+        contact.familyName = familyName
+    }
+    if let middleName = data[EXContactsKeyMiddleName] as! String? {
+        contact.middleName = middleName
+    }
+    if let previousFamilyName = data[EXContactsKeyMaidenName] as! String? {
+        contact.previousFamilyName = previousFamilyName
+    }
+    if let nickname = data[EXContactsKeyNickname] as! String? {
+        contact.nickname = nickname
+    }
+    if let organizationName = data[EXContactsKeyCompany] as! String? {
+        contact.organizationName = organizationName
+    }
+    if let jobTitle = data[EXContactsKeyJobTitle] as! String? {
+        contact.jobTitle = jobTitle
+    }
+    if let departmentName = data[EXContactsKeyDepartment] as! String? {
+        contact.departmentName = departmentName
+    }
+    if let namePrefix = data[EXContactsKeyNamePrefix] as! String? {
+        contact.namePrefix = namePrefix
+    }
+    if let nameSuffix = data[EXContactsKeyNameSuffix] as! String? {
+        contact.nameSuffix = nameSuffix
+    }
+    if let phoneticGivenName = data[EXContactsKeyPhoneticFirstName] as! String? {
+        contact.phoneticGivenName = phoneticGivenName
+    }
+    if let phoneticMiddleName = data[EXContactsKeyPhoneticMiddleName] as! String? {
+        contact.phoneticMiddleName = phoneticMiddleName
+    }
+    if let phoneticFamilyName = data[EXContactsKeyPhoneticLastName] as! String? {
+        contact.phoneticFamilyName = phoneticFamilyName
+    }
+    if let note = data[EXContactsKeyNote] as! String? {
+        contact.note = note
+    }
+    if let birthday = decodeDate(data: data[EXContactsKeyBirthday] as! Dictionary<String, Any>?) {
+        contact.birthday = birthday
+    }
     
     if let phoneNumebrs = data[EXContactsKeyPhoneNumbers] as! Array<Dictionary<String, String>>? {
         contact.phoneNumbers = phoneNumebrs.map{
@@ -84,8 +114,6 @@ func mutateContact(contact: CNMutableContact, data: Dictionary<String, Any>) {
             let number = $0["number"]!
             return CNLabeledValue(label: label, value: CNPhoneNumber(stringValue: number))
         }
-    } else {
-        contact.phoneNumbers = []
     }
     
     if let postalAddresses = data[EXContactsKeyAddresses] as! Array<Dictionary<String, String>>? {
@@ -100,8 +128,6 @@ func mutateContact(contact: CNMutableContact, data: Dictionary<String, Any>) {
             value.isoCountryCode = $0["isoCountryCode"] ?? ""
             return CNLabeledValue(label: label, value: value)
         }
-    } else {
-        contact.postalAddresses = []
     }
     
     if let emailAddresses = data[EXContactsKeyEmails] as! Array<Dictionary<String, String>>? {
@@ -110,38 +136,26 @@ func mutateContact(contact: CNMutableContact, data: Dictionary<String, Any>) {
             let value = $0["email"]!
             return CNLabeledValue(label: label, value: value as NSString)
         }
-    } else {
-        contact.emailAddresses = []
     }
     
     if let instantMessageAddresses = data[EXContactsKeyInstantMessageAddresses] as! Array<Dictionary<String, String>>? {
         contact.instantMessageAddresses = EXContacts.decodeInstantMessageAddresses(instantMessageAddresses) as! [CNLabeledValue<CNInstantMessageAddress>]
-    } else {
-        contact.instantMessageAddresses = []
     }
     
     if let urlAddresses = data[EXContactsKeyUrlAddresses] as! Array<Dictionary<String, String>>? {
         contact.urlAddresses = EXContacts.decodeUrlAddresses(urlAddresses) as! [CNLabeledValue<NSString>]
-    } else {
-        contact.urlAddresses = []
     }
     
     if let dates = data[EXContactsKeyDates] as! Array<Dictionary<String, String>>? {
         contact.dates = EXContacts.decodeDates(dates) as! [CNLabeledValue<NSDateComponents>]
-    } else {
-        contact.dates = []
     }
     
     if let relationships = data[EXContactsKeyRelationships] as! Array<Dictionary<String, String>>? {
         contact.contactRelations = EXContacts.decodeRelationships(relationships) as! [CNLabeledValue<CNContactRelation>]
-    } else {
-        contact.contactRelations = []
     }
     
     if let image = data[EXContactsKeyImage] as! Dictionary<String, String>? {
         let base64 = image["base64"]
         contact.imageData = (base64 != nil) ? Data(base64Encoded: base64!) : nil
-    } else {
-        contact.imageData = nil
     }
 }
