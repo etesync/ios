1be7f3207cc5514ba4d314a4db880a2c3b693c3e
Tom Hacohen <tom@stosb.com>
2020-01-19 19:13:47 +0200

    LoadingIndicator: fix alignment issue.

--- src/widgets/LoadingIndicator.tsx
+++ src/widgets/LoadingIndicator.tsx
@@ -7,7 +7,7 @@ export default function LoadingIndicator(_props: any) {
   return (
     <Container style={{ flexGrow: 1, justifyContent: 'center' }}>
       <ActivityIndicator animating size="large" style={[style, { marginTop: 'auto' }]} {...props} />
-      {status && <Text style={{ textAlign: 'center', marginTop: 30 }}>{status}</Text>}
+      {status && <Text style={{ textAlign: 'center', marginTop: 30, marginBottom: 'auto' }}>{status}</Text>}
       {notice && <Text style={{ marginTop: 'auto' }}>{notice}</Text>}
     </Container>
   );
