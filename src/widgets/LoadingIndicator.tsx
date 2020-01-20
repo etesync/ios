0883f5e3a9c191a30bee44435572e9f68e986f52
Tom Hacohen <tom@stosb.com>
2020-01-20 19:03:06 +0200

    LoadingIndicator: fix alignment issues (again).

--- src/widgets/LoadingIndicator.tsx
+++ src/widgets/LoadingIndicator.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import { View } from 'react-native';
 import { ActivityIndicator, Text } from 'react-native-paper';
 
 import Container from './Container';
 export default function LoadingIndicator(_props: any) {
   const { style, status, notice, ...props } = _props;
   return (
-    <Container style={{ flexGrow: 1, justifyContent: 'center' }}>
-      <ActivityIndicator animating size="large" style={[style, { marginTop: 'auto' }]} {...props} />
-      {status && <Text style={{ textAlign: 'center', marginTop: 30, marginBottom: 'auto' }}>{status}</Text>}
-      {notice && <Text style={{ marginTop: 'auto' }}>{notice}</Text>}
+    <Container style={{ flexGrow: 1 }}>
+      <View style={{ flexGrow: 1, justifyContent: 'center' }}>
+        <ActivityIndicator animating size="large" style={[style]} {...props} />
+        {status && <Text style={{ textAlign: 'center', marginTop: 30 }}>{status}</Text>}
+      </View>
+      {notice && <Text>{notice}</Text>}
     </Container>
   );
 }
