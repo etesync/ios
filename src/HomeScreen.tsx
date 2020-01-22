31be9708b23294c04398b634fc04260ea8bfc21b
Tom Hacohen <tom@stosb.com>
2020-01-22 15:01:39 +0200

    Wizard: run the welcome wizard as the first thing when opening the app.

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,84 +1,37 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavigationScreenComponent } from 'react-navigation';
-import { View } from 'react-native';
-import { Appbar, Paragraph, Title } from 'react-native-paper';
+import { Appbar } from 'react-native-paper';
 
 import { SyncManager } from './sync/SyncManager';
 
 import JournalListScreen from './components/JournalListScreen';
-import { usePermissions, AskForPermissions } from './Permissions';
-import Wizard, { WizardNavigationBar, PagePropsType } from './widgets/Wizard';
+import { usePermissions } from './Permissions';
 
 import { StoreState } from './store';
-import { performSync, setSettings } from './store/actions';
+import { performSync } from './store/actions';
 
 import { useCredentials } from './login';
 import { useSyncGate } from './SyncGate';
 import { registerSyncTask } from './sync/SyncManager';
-import SyncSettings from './sync/SyncSettings';
 
-import * as C from './constants';
-
-const wizardPages = [
-  (props: PagePropsType) => (
-    <>
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Title style={{ textAlign: 'center' }}>Welcome to EteSync!</Title>
-        <Paragraph style={{ textAlign: 'center' }}>
-          Please follow these few quick steps to setup the EteSync app.
-        </Paragraph>
-      </View>
-      <WizardNavigationBar {...props} />
-    </>
-  ),
-  (props: PagePropsType) => (
-    <>
-      <AskForPermissions />
-      <WizardNavigationBar {...props} />
-    </>
-  ),
-];
-
-if (C.syncAppMode) {
-  wizardPages.push(
-    (props: PagePropsType) => (
-      <>
-        <Title>Sync Settings</Title>
-        <Paragraph>
-          EteSync syncs with your device's existing accounts, so you have to choose an account to sync with to before going forward. For example, if you choose the iCloud account, all of your EteSync data will sync with iCloud.
-        </Paragraph>
-        <Paragraph>
-          iOS doesn't expose the "local" account unless all other accounts are disabled. Therefore, in order to only sync EteSync with your device, please first turn off iCloud sync for contacts, calendars and reminders (or only some of them) from the device's Settings app.
-        </Paragraph>
-        <SyncSettings />
-        <WizardNavigationBar {...props} />
-      </>
-    )
-  );
-}
 
 const HomeScreen: NavigationScreenComponent = React.memo(function _HomeScreen() {
   const dispatch = useDispatch();
   const etesync = useCredentials()!;
   const SyncGate = useSyncGate();
-  const settings = useSelector((state: StoreState) => state.settings);
   const permissionsStatus = usePermissions();
 
   React.useEffect(() => {
-    if (etesync && !permissionsStatus && settings.ranWizrd) {
+    if (etesync && !permissionsStatus) {
       registerSyncTask(etesync.credentials.email);
       const syncManager = SyncManager.getManager(etesync);
       dispatch(performSync(syncManager.sync()));
     }
-  }, [etesync, !permissionsStatus, settings.ranWizrd]);
+  }, [etesync, !permissionsStatus]);
 
   if (permissionsStatus) {
     return permissionsStatus;
-  }
-
-  if (!settings.ranWizrd) {
-    return <Wizard pages={wizardPages} onFinish={() => dispatch(setSettings({ ranWizrd: true }))} style={{ flex: 1 }} />;
   }
 
   if (SyncGate) {
