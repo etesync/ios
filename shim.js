095a1fe40f0a120dee39a8309338546d022c1704
Tom Hacohen <tom@stosb.com>
2020-05-07 12:04:57 +0100

    Set up the expo-updates config and fix export

--- shim.js
+++ shim.js
@@ -1,15 +1,6 @@
 if (typeof __dirname === 'undefined') global.__dirname = '/'
 if (typeof __filename === 'undefined') global.__filename = ''
-if (typeof process === 'undefined') {
-  global.process = require('process')
-} else {
-  const bProcess = require('process')
-  for (var p in bProcess) {
-    if (!(p in process)) {
-      process[p] = bProcess[p]
-    }
-  }
-}
+global.process = global.process || {}
 
 process.browser = false
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
