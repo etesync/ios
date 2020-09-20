d9383df8bcef07a4dbdcf456f00a38493fd14f35
Tom Hacohen <tom@stosb.com>
2020-09-20 12:33:17 +0300

    ErrorBoundary: fix handling of UnauthorizedError.

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -147,7 +147,7 @@ function ErrorBoundaryInner(props: React.PropsWithChildren<{ error: Error | unde
 
   let nonFatalErrorDialog;
   if (nonFatalError) {
-    if ((nonFatalError instanceof Etebase.HttpError) && (nonFatalError.status === 401)) {
+    if (nonFatalError instanceof Etebase.UnauthorizedError) {
       nonFatalErrorDialog = (
         <SessionExpiredDialog />
       );
