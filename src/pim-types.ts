f62902d26e83dff0d9f0619eee16410f8917c17b
Tom Hacohen <tom@stosb.com>
2020-12-09 18:46:40 +0200

    pim-types: sync from web client.

--- src/pim-types.ts
+++ src/pim-types.ts
@@ -210,9 +210,14 @@ export class TaskType extends EventType {
 
   set tags(tags: string[]) {
     const property = this.component.getFirstProperty("categories");
+    const empty = tags.length === 0;
     if (property) {
-      property.setValues(tags);
-    } else {
+      if (empty) {
+        this.component.removeAllProperties("categories");
+      } else {
+        property.setValues(tags);
+      }
+    } else if (!empty) {
       const newProp = new ICAL.Property("categories", this.component);
       newProp.setValues(tags);
       this.component.addProperty(newProp);
@@ -220,7 +225,7 @@ export class TaskType extends EventType {
   }
 
   get tags() {
-    return this.component.getFirstProperty("categories")?.getValues() ?? [];
+    return this.component.getFirstProperty("categories")?.getValues()?.filter((x) => x !== "") ?? [];
   }
 
   set dueDate(date: ICAL.Time | undefined) {
